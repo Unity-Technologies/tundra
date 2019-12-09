9ea653d3af1ac8c124c761907fa498bd765ccbec
Lucas Meijer <lucas@unity3d.com>
2019-12-09 15:22:59 +0100

    cleanup

--- src/FileInfo.cpp
+++ src/FileInfo.cpp
@@ -274,16 +274,9 @@ bool DeleteDirectory(const char* path)
     int result = std::filesystem::remove_all(path, error);
     return result != -1;
 #else
-
-
-        auto unlink_cb = [] (const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) -> int
+    auto unlink_cb = [] (const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) -> int
     {
-        if (remove(fpath) == -1)
-        {
-            return FTW_STOP;
-        }
-
-        return FTW_CONTINUE;
+        return remove(fpath) == -1 ? FTW_STOP : FTW_CONTINUE;
     };
 
     if (FTW_STOP == nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS))
