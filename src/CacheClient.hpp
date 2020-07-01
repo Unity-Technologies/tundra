67338dfc46ca1bced6acdcaede82a42a468f1b3e
Lucas Meijer <lucas@unity3d.com>
2020-07-02 00:34:17 +0200

    fix non-windows

--- src/CacheClient.hpp
+++ src/CacheClient.hpp
@@ -8,8 +8,8 @@ struct ThreadState;
 
 struct CacheClient
 {
-    static bool CacheClient::AttemptRead(const Frozen::DagNode* dagNode, HashDigest signature, StatCache* stat_cache, Mutex* queue_lock, ThreadState* thread_state);
-    static bool CacheClient::AttemptWrite(const Frozen::DagNode* dagNode, HashDigest signature, StatCache* stat_cache, Mutex* queue_lock, ThreadState* thread_state);
+    static bool AttemptRead(const Frozen::DagNode* dagNode, HashDigest signature, StatCache* stat_cache, Mutex* queue_lock, ThreadState* thread_state);
+    static bool AttemptWrite(const Frozen::DagNode* dagNode, HashDigest signature, StatCache* stat_cache, Mutex* queue_lock, ThreadState* thread_state);
 };
 
 void GetCachingBehaviourSettingsFromEnvironment(bool* attemptReads, bool* attemptWrites);
