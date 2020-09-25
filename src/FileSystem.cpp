584f60ff83697b72e9bef6b6e5ae7f626b9eee21
Martin Sternevald <martins@unity3d.com>
2020-09-25 22:48:29 +0200

    windows; fix mtime file sampling

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -9,25 +9,19 @@ namespace FileSystem
     volatile uint64_t g_LastSeenFileSystemTime;
 }
 
-static FILE *s_LastSeenFileSystemTimeSampleFile;
+char s_LastSeenFileSystemTimeSampleFile[kMaxPathLength];
 static Mutex s_LastSeenFileSystemTimeLock;
 
 void FileSystemInit(const char *dag_fn)
 {
     MutexInit(&s_LastSeenFileSystemTimeLock);
 
-    char lastSeenFileSystemTimeSampleFile[kMaxPathLength];
-    snprintf(lastSeenFileSystemTimeSampleFile, sizeof lastSeenFileSystemTimeSampleFile, "%s_fsmtime", dag_fn);
-    lastSeenFileSystemTimeSampleFile[sizeof(lastSeenFileSystemTimeSampleFile) - 1] = '\0';
-
-    s_LastSeenFileSystemTimeSampleFile = fopen(lastSeenFileSystemTimeSampleFile, "w");
-    if (s_LastSeenFileSystemTimeSampleFile == nullptr)
-        CroakErrno("Unable to create timestamp file '%s'", lastSeenFileSystemTimeSampleFile);
+    snprintf(s_LastSeenFileSystemTimeSampleFile, sizeof s_LastSeenFileSystemTimeSampleFile, "%s_fsmtime", dag_fn);
+    s_LastSeenFileSystemTimeSampleFile[sizeof(s_LastSeenFileSystemTimeSampleFile) - 1] = '\0';
 }
 
 void FileSystemDestroy()
 {
-    fclose(s_LastSeenFileSystemTimeSampleFile);
     MutexDestroy(&s_LastSeenFileSystemTimeLock);
 }
 
@@ -36,39 +30,26 @@ uint64_t FileSystemUpdateLastSeenFileSystemTime()
     MutexLock(&s_LastSeenFileSystemTimeLock);
 
     uint64_t valueToWrite = FileSystem::g_LastSeenFileSystemTime; // not important what we write, just that we write something.
+    FILE* lastSeenFileSystemTimeSampleFileFd = fopen(s_LastSeenFileSystemTimeSampleFile, "w");
+    if (lastSeenFileSystemTimeSampleFileFd == nullptr)
+        CroakErrno("Unable to create timestamp file '%s'", lastSeenFileSystemTimeSampleFileFd);
 
-    rewind(s_LastSeenFileSystemTimeSampleFile);
-    fwrite(&valueToWrite, sizeof valueToWrite, 1, s_LastSeenFileSystemTimeSampleFile);
-    fflush(s_LastSeenFileSystemTimeSampleFile);
+    fwrite(&valueToWrite, sizeof valueToWrite, 1, lastSeenFileSystemTimeSampleFileFd);
+    fclose(lastSeenFileSystemTimeSampleFileFd);
 
-#if defined(TUNDRA_UNIX)
-    struct stat fileInformation;
-    if (fstat(fileno(s_LastSeenFileSystemTimeSampleFile), &fileInformation) != 0)
-        CroakErrno("Unable to read file timestamp");
-#elif defined(TUNDRA_WIN32)
-    struct __stat64 fileInformation;
-    if (_fstat64(fileno(s_LastSeenFileSystemTimeSampleFile), &fileInformation) != 0)
-        CroakErrno("Unable to read file timestamp");
-#endif
-    FileSystem::g_LastSeenFileSystemTime = fileInformation.st_mtime;
+    FileSystem::g_LastSeenFileSystemTime = GetFileInfo(s_LastSeenFileSystemTimeSampleFile).m_Timestamp;
     MutexUnlock(&s_LastSeenFileSystemTimeLock);
-
     return FileSystem::g_LastSeenFileSystemTime;
 }
 
 #if TUNDRA_UNIX
-#include <unistd.h> // usleep
+#include <unistd.h>
+#define Sleep(x) usleep(x * 1000)
 #endif
 
 void FileSystemWaitUntilFileModificationDateIsInThePast(uint64_t timeThatNeedsToBeInThePast)
 {
     // Wait for next file system tick
     while (timeThatNeedsToBeInThePast >= FileSystemUpdateLastSeenFileSystemTime())
-    {
-#if TUNDRA_WIN32
         Sleep(100);
-#else
-        usleep(100000);
-#endif
-    }
 }
